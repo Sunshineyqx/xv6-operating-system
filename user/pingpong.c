60532d609e283876f13b27f5dbddebf5bacb3863
sunshine <3275316624@qq.com>
2024-01-14 12:55:03 +0800

    modify user/pingpong.c (add wait()..)

--- user/pingpong.c
+++ user/pingpong.c
@@ -21,14 +21,14 @@ main(int argc, char* argv[]){
             fprintf(2, "failed to read in child\n");
             exit(1);
         };
+        close(pipe2[0]);
         fprintf(1, "%d: received ping\n", pid);
         if(write(pipe1[1], buf, 1) != 1){
             fprintf(2, "failed to write in child\n");
             exit(1);
-        };
-
+        }
         close(pipe1[1]);
-        close(pipe2[0]);
+        
         exit(0);
     }
     else //parent
@@ -40,15 +40,15 @@ main(int argc, char* argv[]){
         if(write(pipe2[1], buf, 1) != 1){
             fprintf(2, "failed to write in parent\n");
             exit(1);
-        };
+        }
+        close(pipe2[1]);
+        wait(0);
         if(read(pipe1[0], buf, 1) != 1){
             fprintf(2, "failed to read in parent\n");
             exit(1);
         }
         fprintf(1, "%d: received pong\n", pid);
-
         close(pipe1[0]);
-        close(pipe2[1]);
         exit(1);
     }
 
