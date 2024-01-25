30164d70d078c0f3523d43a5aab0643f43e90089
sunshine <3275316624@qq.com>
2024-01-25 18:11:11 +0800

    backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,6 +121,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -131,4 +132,18 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+// 打印每个栈帧的地址
+void
+backtrace(){
+  uint64 cur_fp = r_fp();
+  uint64 bottom = PGROUNDDOWN(cur_fp);
+  uint64 top = PGROUNDUP(cur_fp);
+  while(cur_fp < top && cur_fp > bottom){
+    uint64 ret = *(uint64 *)(cur_fp - 8);
+    printf("%p\n", ret);
+    uint64  pre_fp = *(uint64 *)(cur_fp - 16);
+    cur_fp = pre_fp;
+  }
 }
