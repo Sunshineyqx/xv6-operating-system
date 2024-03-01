43585a2dddc9e554565f602a7554cdbd4ec73942
sunshine <3275316624@qq.com>
2024-03-01 15:52:28 +0800

    ok

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -316,6 +316,32 @@ sys_open(void)
     }
   }
 
+  if(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)){
+      int depth = 15;
+      char path[MAXPATH];
+      for(int i = 0; i < depth; i++){
+        if((readi(ip, 0, (uint64)path, 0, MAXPATH)) != MAXPATH){
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+
+        iunlockput(ip);
+        ip = namei(path);
+        if(ip == 0){
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+        if(ip->type != T_SYMLINK) break;
+        if(i == depth -1){ //递归次数太多
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+      }
+  }
+
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
     end_op();
@@ -482,5 +508,32 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void){
+  char target[MAXPATH];
+  char path[MAXPATH];
+  if((argstr(0, target, MAXPATH) < 0) || (argstr(1, path, MAXPATH)) < 0){
+      return -1;
+  }
+
+  struct inode* ip;
+  begin_op();
+
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op();
+    return -1;
+  }
+
+  if((writei(ip, 0, (uint64)target, 0, MAXPATH)) < MAXPATH){
+    iunlockput(ip);
+    end_op();
+    return  -1;
+  }
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
